962fd63ad90c37cf698f0a0d6c92f96c37e80e0c
ethvor <contactethanv@gmail.com>
2025-11-25 19:43:32 -0600

    add pokeapi integration and pokemon display

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.github.amitshekhariitbhu.Fast-Android-Networking:android-networking:1.0.4")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/ethvor/pokemonwatchlist2/MainActivity.java
+++ app/src/main/java/com/ethvor/pokemonwatchlist2/MainActivity.java
@@ -1,24 +1,155 @@
- package com.ethvor.pokemonwatchlist2;
+package com.ethvor.pokemonwatchlist2;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.BitmapRequestListener;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText searchInput;
+    private Button searchButton;
+    private TextView pokemonName;
+    private TextView pokemonNumber;
+    private TextView pokemonWeight;
+    private TextView pokemonHeight;
+    private TextView pokemonBaseXp;
+    private TextView pokemonMove;
+    private TextView pokemonAbility;
+    private ImageView pokemonImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        AndroidNetworking.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        searchInput = findViewById(R.id.searchInput);
+        searchButton = findViewById(R.id.searchButton);
+        pokemonName = findViewById(R.id.pokemonName);
+        pokemonNumber = findViewById(R.id.pokemonNumber);
+        pokemonWeight = findViewById(R.id.pokemonWeight);
+        pokemonHeight = findViewById(R.id.pokemonHeight);
+        pokemonBaseXp = findViewById(R.id.pokemonBaseXp);
+        pokemonMove = findViewById(R.id.pokemonMove);
+        pokemonAbility = findViewById(R.id.pokemonAbility);
+        pokemonImage = findViewById(R.id.pokemonImage);
+
+        searchButton.setOnClickListener(v -> {
+            String input = searchInput.getText().toString().toLowerCase();
+
+            if (!isValidInput(input)) {
+                Toast.makeText(this, "Invalid pokemon name or id", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            fetchPokemon(input);
         });
+    }
+
+    //get pokemon from api
+    private void fetchPokemon(String pokemon) {
+        AndroidNetworking.get("https://pokeapi.co/api/v2/pokemon/{pokemon}")
+                .addPathParameter("pokemon", pokemon)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        displayPokemon(response);
+                    }
+
+                    @Override
+                    public void onError(ANError error) {
+                        Toast.makeText(MainActivity.this, "Pokemon not found", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void displayPokemon(JSONObject response) {
+        try {
+            String name = response.getString("name");
+            int id = response.getInt("id");
+            int weight = response.getInt("weight");
+            int height = response.getInt("height");
+            int baseXp = response.getInt("base_experience");
+
+            JSONArray moves = response.getJSONArray("moves");
+            String move = moves.getJSONObject(0).getJSONObject("move").getString("name");
+
+            JSONArray abilities = response.getJSONArray("abilities");
+            String ability = abilities.getJSONObject(0).getJSONObject("ability").getString("name");
+
+            String imageUrl = response.getJSONObject("sprites").getString("front_default");
+
+            pokemonName.setText(name);
+            pokemonNumber.setText("Number: " + id);
+            pokemonWeight.setText("Weight: " + weight);
+            pokemonHeight.setText("Height: " + height);
+            pokemonBaseXp.setText("Base XP: " + baseXp);
+            pokemonMove.setText("Move: " + move);
+            pokemonAbility.setText("Ability: " + ability);
+
+            loadImage(imageUrl);
+        } catch (Exception e) {
+        }
+    }
+
+    private void loadImage(String imageUrl) {
+        AndroidNetworking.get(imageUrl)
+                .build()
+                .getAsBitmap(new BitmapRequestListener() {
+                    @Override
+                    public void onResponse(Bitmap bitmap) {
+                        pokemonImage.setImageBitmap(bitmap);
+                    }
+
+                    @Override
+                    public void onError(ANError error) {
+                    }
+                });
+    }
+
+    private boolean isValidInput(String input) {
+        if (input.isEmpty()) {
+            return false;
+        }
+
+        String invalidChars = "%&*(@)!;:<>";
+        for (int i = 0; i < input.length(); i++) {
+            if (invalidChars.indexOf(input.charAt(i)) >= 0) {
+                return false;
+            }
+        }
+
+        // check if number
+        boolean isNumeric = true;
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                isNumeric = false;
+                break;
+            }
+        }
+
+        if (isNumeric) {
+            int id = Integer.parseInt(input);
+            if (id < 1 || id > 1010) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
